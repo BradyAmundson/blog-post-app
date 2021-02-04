fbe7dd94734004b3bf8adc7edce2c163ac33d783
Brady Amundson <bradyamundson@Bradys-Air.lan>
2021-02-03 17:25:30 -0700

    Added Routers and Basic Style

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import {Container, Row, Col} from 'react-bootstrap'
 import PostForm from './PostForm'
 import PostList from './PostList'
 import PostModal from './PostModal'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [posts, setPosts] = useState([])
@@ -20,6 +26,10 @@ function App() {
    })
   },[setLoading, setPosts])
 
+  useEffect(() => {
+    document.title = "Brady Blog App"
+ }, [])
+
   function addPostToList(newPost) {
     setPosts([newPost, ...posts])
   }
@@ -33,12 +43,13 @@ function App() {
   function closePostModal(){
     setShow(false)
   }
-
-  return (
-<Container>
-<Row className="justify-content-md-center" style={{ marginTop: 60 }}>
+  function Home() {
+    return (
+    <Container>
+      <h2 style={{ textAlign: 'center', fontWeight: "bold"}}>Home</h2>
+    <Row className="justify-content-md-center" style={{ marginTop: 60 }}>
     <Col xs lg="12">
-      <h1 style={{ textAlign: 'center' }}>Brady Notes</h1>
+      <h1 style={{ textAlign: 'center', backgroundColor: '#184a45ff', color: '#FFF', padding: '10px' , borderRadius: '25px' }}>Brady's Notes</h1>
     </Col>
   </Row>
   <Row className="justify-content-md-center" style={{ marginTop: 20 }}>
@@ -52,6 +63,48 @@ function App() {
     </Col>
   </Row>
   <PostModal closePostModal={closePostModal} show={show} postId={postId} />
+</Container>);
+  }
+  
+  function About() {
+    return (
+      <Container>
+    <h2 style={{ textAlign: 'center', fontWeight: "bold"}}>About</h2>
+    </Container>);
+  }
+  
+  function Users() {
+    return (<h2 style={{ textAlign: 'center', fontWeight: "bold"}}>Users</h2>);
+  }
+
+  return (
+<Container style={{ backgroundColor: '#FEA2A2', fontFamily: 'charcoal' }}>
+  <Row>
+  <Router>
+      <div>
+        <nav>
+          <row>
+              <h1 style={{ textAlign: 'center', display: 'flex', justifyContent: 'space-between', padding: '10px' }}>
+              <Link to="/" style={{ backgroundColor: '#182a45ff',color: '#FFF', padding: '10px' , borderRadius: '25px' }}>Home</Link>
+              <Link to="/about" style={{ backgroundColor: '#182a45ff',color: '#FFF', padding: '10px' , borderRadius: '25px' }}>About</Link>
+              <Link to="/users" style={{ backgroundColor: '#182a45ff',color: '#FFF', padding: '10px' , borderRadius: '25px' }}>Users</Link>
+              </h1>
+          </row>
+        </nav>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  </Row>
 </Container>
   );
 }
